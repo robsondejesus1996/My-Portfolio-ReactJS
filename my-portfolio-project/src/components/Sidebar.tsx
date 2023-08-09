347af4289dcfa06252b0a3e73a842046e94415f4
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-08-09 14:45:39 -0300

    Ajuste link cv

--- my-portfolio-project/src/components/Sidebar.tsx
+++ my-portfolio-project/src/components/Sidebar.tsx
@@ -1,24 +1,26 @@
-import SocialNetworks from './SocialNetworks'
-import Avatar from '../img/eu.jpg'
-import InformationContainer from "./InformationContainer"
+import SocialNetworks from "./SocialNetworks";
+import Avatar from "../img/eu.jpg";
+import InformationContainer from "./InformationContainer";
 
-import '../styles/components/sidebar.sass'
-
+import "../styles/components/sidebar.sass";
 
 const Sidebar = () => {
-    return (
-       <aside id="sidebar">
-            <img src={Avatar} alt="Robson de Jesus Imagem" />
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Robson de Jesus Imagem" />
 
-            <p className="title">Engenheiro de Software</p>
-            <SocialNetworks/>
-            <InformationContainer />
-            <a href="" className="btn">
-                Download currículo
-            </a>
-       </aside>
-    )
-}
+      <p className="title">Engenheiro de Software</p>
+      <SocialNetworks />
+      <InformationContainer />
+      <a
+        href="https://www.canva.com/design/DAFo41dboxk/v1idMxvPooYWOm4Wj_F0kA/view?utm_content=DAFo41dboxk&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+        className="btn"
+        target="_blank"
+      >
+        Download currículo
+      </a>
+    </aside>
+  );
+};
 
-
-export default Sidebar
+export default Sidebar;
