fdfbd909c7f3757e4a0b6d5072872717de66ad94
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-08-09 11:16:47 -0300

    Atualização componente sobre mim

--- my-portfolio-project/src/components/AbountContainer.tsx
+++ my-portfolio-project/src/components/AbountContainer.tsx
@@ -1,7 +1,23 @@
-const AbountContainer = () =>{
-    return (
-        <div>AbountContainer</div>
-    )
-}
+const AbountContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quas
+        inventore totam. Repellat perferendis earum, deleniti incidunt magnam
+        ipsum saepe iure non odit omnis sunt ut, consequatur, ullam quod
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero
+        doloribus nostrum corrupti consequuntur, quaerat ipsam laborum aliquid
+        saepe veritatis vel veniam molestiae debitis ullam commodi et vero,
+        incidunt excepturi recusandae non odio quis rem est ex inventore?
+        Distinctio asperiores, ab veritatis, consequuntur maxime inventore
+        nostrum, animi expedita explicabo similique vitae.
+      </p>
+    </section>
+  );
+};
 
-export default AbountContainer
+export default AbountContainer;
