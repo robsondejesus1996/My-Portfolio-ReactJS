ba3cb26d9a68f657301362ea28d2a9825f1cff61
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-08-15 14:31:03 -0300

    Atualização teste

--- my-portfolio-project/src/components/AbountContainer.tsx
+++ my-portfolio-project/src/components/AbountContainer.tsx
@@ -3,6 +3,15 @@ const AbountContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
+        Busco oportunidades na área de Data Science e Desenvolvimento de
+        Sistemas, onde possa aplicar meus conhecimentos em programação, análise
+        de dados e desenvolvimento de soluções tecnológicas inovadoras. Com uma
+        sólida base em linguagens de programação, frameworks e ferramentas de
+        desenvolvimento, juntamente com minha formação em Engenharia de Software
+        e experiência em programação e análise de sistemas, estou comprometido
+        em contribuir para projetos desafiadores e colaborativos.
+      </p>
+      <p>
         Engenheiro de software apaixonado por inovação e desenvolvimento.
         Formado em engenharia de software e com duas pós-graduações em IA e
         desenvolvimento full stack em andamento. Habilidades sólidas em
